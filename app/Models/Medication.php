3a80cf5ea519e2d21b3b75968ba56f75270a486e
Khadija Drider <160004174+KhadijaDr@users.noreply.github.com>
2025-08-04 21:44:25 +0100

    Update Medication.php

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -11,11 +11,10 @@ class Medication extends Model
     protected $table= 'medications';
 
     protected $fillable = ['name', 'description', 'expiry_date', 'supplier', 'quantity', 'price', 'image'];
-
-    // العلاقة بين الأدوية والطلبات
+    
     public function orders()
     {
-        return $this->belongsToMany(Order::class, 'order_medication') // تحديد اسم الجدول الوسيط
+        return $this->belongsToMany(Order::class, 'order_medication') 
                     ->withPivot('quantity');
     }
 }
