398ed1707238e6835b1c7f0a8e6c9f545eebff67
Khadija Drider <160004174+KhadijaDr@users.noreply.github.com>
2025-08-04 22:22:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',  // إذا كان لديك ملف HTML في الجذر
-    './src/**/*.{html,js,vue}',  // لتغطية ملفات HTML و Vue و JS داخل مجلد src
+    './index.html',  
+    './src/**/*.{html,js,vue}',
   ],
   theme: {
     extend: {},
