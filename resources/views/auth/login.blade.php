9ab259eeebec07a8dbe3234ff04e723bbefe39b3
Khadija Drider <160004174+KhadijaDr@users.noreply.github.com>
2025-08-04 21:52:22 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -343,8 +343,6 @@
             <p>PharmaLink - Votre pharmacie en ligne de confiance</p>
         </div>
     </div>
-
-    <!-- ربط مكتبات Bootstrap و JS -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script>
